c7e5662b91be23ee8431e26bf2ca87967ddb6554
palewire <ben.welsh@gmail.com>
2013-11-01 13:33:35 -0700

    Added some docs

--- merge.py
+++ merge.py
@@ -18,9 +18,13 @@ class PalmeroFTW(object):
     outcsv_path = "merged_totals.csv"
 
     def transform(self):
+        # Open the CSV
         general_csv = csv.DictReader(open(self.general_csv_path, 'r'))
+        # Loop through the rows
         grouped_by_precinct = {}
         for row in general_csv:
+            # And regroup them so each fake id is keyed to
+            # all of the list totals for that precinct
             fake_id = "%s-%s" % (row['mesa_desde'], row['mesa_hasta'])
             try:
                 grouped_by_precinct[fake_id][row['vot_parcodigo']] = row['total']
@@ -28,24 +32,33 @@ class PalmeroFTW(object):
                 grouped_by_precinct[fake_id] = {
                     row['vot_parcodigo']: row['total']
                 }
-        grouped_by_precinct_with_totals = []
+        # Now loop through that
         outrows = []
         for fake_id, totals in grouped_by_precinct.items():
+            # Figure out the overall total of votes
             overall_total = sum(map(int, totals.values()))
+            # Rank the lists from highest to lowest votes
             ranking = sorted(
                 totals.items(),
                 key=lambda x:int(x[1]),
                 reverse=True
             )
+            # Pull out the leader and their vote total
             leader, leader_total = ranking[0]
+            # Calculate their margin of victory over the second place list
             margin_of_victory = calculate.margin_of_victory([int(i[1]) for i in ranking])
+            # Start up a row to print out
             outrow = [fake_id,]
+            # Load in the lists in the same "alphabetical" order
             for list_, total in sorted(totals.items(), key=lambda x:x[0]):
                 outrow.append(total)
+            # Load in the extra stuff we've calculated
             outrow.append(leader)
             outrow.append(leader_total)
             outrow.append(margin_of_victory)
+            # Add this row to the global list outside the loop
             outrows.append(outrow)
+        # Open up a text file and write out all the data
         outcsv = csv.writer(open(self.outcsv_path, 'w'))
         outcsv.writerow(self.outheaders)
         outcsv.writerows(outrows)
@@ -53,7 +66,8 @@ class PalmeroFTW(object):
 
 if __name__ == '__main__':
     pftw = PalmeroFTW()
-    pftw.transform()
+    #pftw.transform()
+    pftw.merge()
 
 """
 {'cartodb_id': '4842',
