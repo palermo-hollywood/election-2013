69cd2ef51fba4891c690f27969de43898206a6b0
palewire <ben.welsh@gmail.com>
2013-11-02 09:58:36 -0700

    Wrote some text that mostly sucks

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html',
-        headline = "Headline goes here",
+        headline = "Resultados de las elecciones en la escuela donde votaste",
         description = """
-<p>This is lorem ipsum. Pay no attention. This is lorem ipsum. 
-Pay no attention. This is lorem ipsum. Pay no attention.</p>
+<p>The results of October's deputies election at the schools where
+<em>Capital Federal</em> voters cast ballots.</p>
+
+<p>Circles are colored in to represent
+that party that won the most votes and sized to show the margin of victory.</p>
+
+<p>
+    This is a modified take on <a href="http://www.lanacion.com.ar/1633333-como-fueron-los-resultados-de-las-elecciones-en-la-escuela-donde-votaste">a map</a> published by <em>La Nacion</em> 
+    last week.
+    It was developed by Team Hollywood Palermo at <a href="http://blogs.lanacion.com.ar/datafest/">Datafest 2013</a> in Buenos Aires.
+    The source is <a href="https://github.com/palermo-hollywood/election-2013">available on GitHub</a>.
+</p>
 """
     )
 
